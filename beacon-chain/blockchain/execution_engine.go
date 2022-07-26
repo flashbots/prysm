7726c3b767281d9322f4e866a94979a9c9600e51
Mateusz Morusiewicz <11313015+Ruteri@users.noreply.github.com>
2022-07-26 10:43:18 +0200

    Force sending FCU for all slots

--- beacon-chain/blockchain/execution_engine.go
+++ beacon-chain/blockchain/execution_engine.go
@@ -287,10 +287,7 @@ func (s *Service) optimisticCandidateBlock(ctx context.Context, blk interfaces.B
 // getPayloadAttributes returns the payload attributes for the given state and slot.
 // The attribute is required to initiate a payload build process in the context of an `engine_forkchoiceUpdated` call.
 func (s *Service) getPayloadAttribute(ctx context.Context, st state.BeaconState, slot types.Slot) (bool, *enginev1.PayloadAttributes, types.ValidatorIndex, error) {
-	proposerID, _, ok := s.cfg.ProposerSlotIndexCache.GetProposerPayloadIDs(slot)
-	if !ok { // There's no need to build attribute if there is no proposer for slot.
-		return false, nil, 0, nil
-	}
+	proposerID, _, proposerOk := s.cfg.ProposerSlotIndexCache.GetProposerPayloadIDs(slot)
 
 	// Get previous randao.
 	st = st.Copy()
@@ -305,22 +302,24 @@ func (s *Service) getPayloadAttribute(ctx context.Context, st state.BeaconState,
 
 	// Get fee recipient.
 	feeRecipient := params.BeaconConfig().DefaultFeeRecipient
-	recipient, err := s.cfg.BeaconDB.FeeRecipientByValidatorID(ctx, proposerID)
-	switch {
-	case errors.Is(err, kv.ErrNotFoundFeeRecipient):
-		if feeRecipient.String() == params.BeaconConfig().EthBurnAddressHex {
-			logrus.WithFields(logrus.Fields{
-				"validatorIndex": proposerID,
-				"burnAddress":    params.BeaconConfig().EthBurnAddressHex,
-			}).Warn("Fee recipient is currently using the burn address, " +
-				"you will not be rewarded transaction fees on this setting. " +
-				"Please set a different eth address as the fee recipient. " +
-				"Please refer to our documentation for instructions")
+	if proposerOk {
+		recipient, err := s.cfg.BeaconDB.FeeRecipientByValidatorID(ctx, proposerID)
+		switch {
+		case errors.Is(err, kv.ErrNotFoundFeeRecipient):
+			if feeRecipient.String() == params.BeaconConfig().EthBurnAddressHex {
+				logrus.WithFields(logrus.Fields{
+					"validatorIndex": proposerID,
+					"burnAddress":    params.BeaconConfig().EthBurnAddressHex,
+				}).Warn("Fee recipient is currently using the burn address, " +
+					"you will not be rewarded transaction fees on this setting. " +
+					"Please set a different eth address as the fee recipient. " +
+					"Please refer to our documentation for instructions")
+			}
+		case err != nil:
+			return false, nil, 0, errors.Wrap(err, "could not get fee recipient in db")
+		default:
+			feeRecipient = recipient
 		}
-	case err != nil:
-		return false, nil, 0, errors.Wrap(err, "could not get fee recipient in db")
-	default:
-		feeRecipient = recipient
 	}
 
 	// Get timestamp.
